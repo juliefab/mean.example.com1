b19417b63a12c91d7face511baf32ca3dba43506
juliefab <jfinch1@artic.edu>
2018-01-09 16:53:08 -0600

    initial commit - basic API workflow

---
+++ public/javascripts/main.js
@@ -0,0 +1,47 @@
+var url ="http://localhost:3000/api/users";
+
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', url);
+xhr.send();
+
+xhr.onload = function() {
+
+let data = JSON.parse(xhr.response);
+// loop through the users
+var rows = '';
+for(user in data){
+ let a=data[user]._id;
+ let b=data[user].name;
+ let c=data[user].email;
+
+// action for each user
+ rows = rows + `<tr><td><a href="#${user}" onclick="send('${user}');">${a}</a></td> <td>${b}</td> <td>${c}</td></tr>`;
+  }
+
+var h1 = document.getElementById('title');
+var table = document.createElement('table');
+h1.appendChild(table).innerHTML= rows;
+
+}
+
+
+function send(who){
+
+    var url = 'http://localhost:3000/api/users/view/' + who;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+
+xhr.onload = function() {
+
+  let data = JSON.parse(xhr.response);
+
+
+  document.getElementById('id').innerHTML = data._id;
+  document.getElementById('name').innerHTML = data.name;
+  document.getElementById('email').innerHTML = data.email;
+  }
+
+}
